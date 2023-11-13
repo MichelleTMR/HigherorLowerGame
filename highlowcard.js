e501db2a663f97beb3c49edfb559fd0e002c38a2
Michelle Munoz Ruiz <mmunozruiz@mkc02dp783md6r.humad.com>
2023-11-13 16:13:18 -0500

    updated highlow game

--- highlowcard.js
+++ highlowcard.js
@@ -1,10 +1,3 @@
-//You are tasked with building a Higher or Lower Guessing Game! 
- //- The application should allow a user to: submit a number as their guess and receive response that their guess needs to be "higher" or "lower". 
- //- When the user guesses the correct number, the app should congratulate them.
- 
- //Bonus: 
- //Generate a random number every time the users plays the game.
-
 let randomNumber;
 resetGame()
 
